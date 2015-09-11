cf430b30b07e38c99d7af4eb9ae78b87e47d1b70
Anagha Joshi <anagha.elex@gmail.com>
2015-09-11 18:02:39 -0400

    Add Palindrome checking code

---
+++ palindrome_check.js
@@ -0,0 +1,37 @@
+// Returns the first character of the string str
+var firstCharacter = function(str) {
+    return str.slice(0, 1);
+};
+
+// Returns the last character of a string str
+var lastCharacter = function(str) {
+    return str.slice(-1);
+};
+
+// Returns the string that results from removing the first
+//  and last characters from str
+var middleCharacters = function(str) {
+    return str.slice(1, -1);
+};
+
+var isPalindrome = function(str) {
+    // base case #1
+    // base case #2
+    if (str.length <=1){
+        return true;
+    }
+    // recursive case
+    if (firstCharacter(str) !== lastCharacter(str)){
+        return false;
+    }
+    return isPalindrome(middleCharacters(str));
+};
+
+var checkPalindrome = function(str) {
+    println("Is this word a palindrome? " + str);
+    println(isPalindrome(str));
+};
+
+checkPalindrome("a");
+checkPalindrome("motor");
+checkPalindrome("rotor");
